2c6c41c157abad027d7d9cd6104707c45352ec91
Abrahamcepe <abrahamgolf@gmail.com>
2020-09-21 15:28:21 -0500

    Added song duration

--- src/SongRow.js
+++ src/SongRow.js
@@ -2,15 +2,36 @@ import React from 'react';
 import './SongRow.css';
 
 function SongRow({track}) {
+    console.log(track);
     return (
       <div className="songRow">
-        <img className="songRow__album" src={track.album.images[0].url} alt="" />
+        <img
+          className="songRow__album"
+          src={track.album.images[0].url}
+          alt=""
+        />
         <div className="songRow__info">
           <h1>{track.name}</h1>
           <p>
             {track.artists.map((artist) => artist.name).join(", ")}
             {" • "}
             {track.album.name}
+          </p>
+        </div>
+        <div className="songRow__duration">
+          <p>
+            {Math.floor(track.duration_ms * 0.00001667)}
+            {":"}
+            {Math.floor(
+              (track.duration_ms -
+                Math.floor(track.duration_ms * 0.00001667) / 0.00001667) *
+                0.001
+            ) < 10 && "0"}
+            {Math.floor(
+              (track.duration_ms -
+                Math.floor(track.duration_ms * 0.00001667) / 0.00001667) *
+                0.001
+            )}
           </p>
         </div>
       </div>
